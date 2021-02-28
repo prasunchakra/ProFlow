25acf5a106cb45a4ef62120f80e18b45a6e8cca1
prasunchakra <lordprasun@gmail.com>
2021-02-28 09:07:21 +0530

    Adding one dynamic component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
-import ToDoRowItem from './components/ToDoRowItem'
+import ToDoComponent from './components/ToDoComponent';
 import './App.css';
 
 function App() {
   const todos = ['Java', 'C++', 'JAvaScript']
   return (
     <div className="container mt-5">
-      <div className="card">
-        <div className="card-header bg-primary text-white">
-          <h3>To-Do List</h3>
-        </div>
-        <ul className="list-group list-group-flush">
-          <ToDoRowItem task={ todos[0]} />
-          <ToDoRowItem task={todos[1]} />
-          <ToDoRowItem task={ todos[2]} />
-        </ul>
-        <div className="card-footer text-muted">
-          <button className="btn btn-success">Mark All Complete</button>
-        </div>
-      </div>
+      <ToDoComponent todos={todos}></ToDoComponent>
     </div>
   );
 }
---
+++ src/components/ToDoComponent.js
@@ -0,0 +1,22 @@
+import ToDoRowItem from "./ToDoRowItem";
+
+function ToDoComponent(props) {
+    return (
+        <div className="card">
+            <div className="card-header bg-primary text-white">
+                <h3>To-Do List</h3>
+                </div>
+            <ul className="list-group list-group-flush">
+                {
+                    props.todos.map(todo => (
+                        <ToDoRowItem task={todo} />
+                    ))
+                }   
+            </ul>
+            <div className="card-footer text-muted">
+            <button className="btn btn-success">Mark All Complete</button>
+            </div>
+        </div>
+    )
+}
+export default ToDoComponent;
